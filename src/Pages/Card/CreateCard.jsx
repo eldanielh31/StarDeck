f9fc65eada857397e1f6b0a39629211e014fd996
Daniel <danbg31@gmail.com>
2023-04-23 02:50:45 -0600

    terminada pagina crear carta ✅

--- src/Pages/Card/CreateCard.jsx
+++ src/Pages/Card/CreateCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import "./CreateCard.css"
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid"
+
 
 function CreateCard() {
+
+    const types = [
+        { value: 'ultra-rara', label: 'Ultra-Rara' },
+        { value: 'muy-rara', label: 'Muy Rara' },
+        { value: 'rara', label: 'Rara' },
+        { value: 'normal', label: 'Normal' },
+        { value: 'basica', label: 'Básica' },
+    ]
+
+    const races = [
+        { value: 'elfo', label: 'Elfo' },
+        { value: 'paladin', label: 'Paladin' },
+        { value: 'barbaro', label: 'Barbaro' },
+    ]
 
     const [name, setName] = useState('');
     const [type, setType] = useState('');
@@ -11,73 +27,84 @@ function CreateCard() {
     const [race, setRace] = useState('');
     const [cost, setCost] = useState('');
     const [energy, setEnergy] = useState('');
-    const [attribute, setAttribute] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Ok")
+        const codigo = 'C-' + uuidv4().split('').reverse().join('').slice(0, 12);
+        console.log(codigo)
+        const attribute = 'activo'
+        console.log([name, type, image, description, race, cost, energy, attribute]);
     }
 
     return (
         <div className="container">
             <div className="content">
-                <div className="sidebar">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="https://via.placeholder.com/200x150.png" alt="Imagen de la carta"/>
-                        </div>
-                        <div class="card-details">
-                            <h2>Título de la carta</h2>
-                            <p>Descripción de la carta</p>
-                            <ul>
-                                <li>Raza: Variable 1</li>
-                                <li>Costo: <span class="cost">Variable 2</span></li>
-                                <li>Energía: Variable 3</li>
-                                <li>Tipo: Variable 4</li>
-                                <li>Atributo: Variable 5</li>
+                <div className="containerCard">
+                    <div className="card-content">
+                        <h3>{name ? name : 'Nombre de Carta'}</h3>
+                        <img className="image-box" src="https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg" alt="dragon-by-Ciruelo" />
+                        <h4>{type ? type : 'Tipo'}</h4>
+                        <div className="info-box">
+                            <ul className="list">
+                                <li><span className="list-item-title">Raza: </span>{race ? race : '...'}</li>
+                                <li><span className="list-item-title">Costo: </span>{cost ? cost : '...'}</li>
+                                <li><span className="list-item-title">Energy: </span>{energy ? energy : '...'}</li>
                             </ul>
+                            <p className="description">{description ? description : 'Descripción de la carta'}</p>
+                            <p className="credits" align="right">C-XXXXXXXXXXXX</p>
                         </div>
                     </div>
+
                 </div>
-                <div className="main">
-                    <h1>Crear carta</h1>
-                    <div className='containerFrom'>
-                        <form onSubmit={handleSubmit}>
-                            <div className='containerInputs'>
-                                <label htmlFor="name">Nombre:</label>
-                                <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="image">Imagen:</label>
-                                <input type="text" id="image" value={image} onChange={(e) => setImage(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="description">Descripción:</label>
-                                <textarea id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="description">Tipo:</label>
-                                <input id="text" value={type} onChange={(e) => setType(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="race">Raza:</label>
-                                <input id="text" value={race} onChange={(e) => setRace(e.target.value)} />
-                            </div><div>
-                                <label htmlFor="cost">Costo:</label>
-                                <input id="number" value={cost} onChange={(e) => setCost(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="energy">Energia:</label>
-                                <input id="number" value={energy} onChange={(e) => setEnergy(e.target.value)} />
-                            </div>
-                            <div>
-                                <label htmlFor="attribute">Atributo:</label>
-                                <input id="text" value={attribute} onChange={(e) => setAttribute(e.target.value)} />
-                            </div>
-                            
-                            <button type="submit">Crear carta</button>
-                        </form>
+                <div className="containerForm">
+                    <div className='wrapperForm'>
+                        <h1>Crear carta</h1>
+                        <div className='containerFrom'>
+                            <form onSubmit={handleSubmit}>
+                                <div className='containerInputs'>
+                                    <label htmlFor="name" className='inputCard'>Nombre:</label>
+                                    <input type="text" id="name" value={name} minLength={5} maxLength={30} onChange={(e) => setName(e.target.value)} required />
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="image" className='inputCard'>Imagen:</label>
+                                    <input type="text" id="image" value={image} onChange={(e) => setImage(e.target.value)} required />
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="description" className='inputCard'>Descripción:</label>
+                                    <textarea id="description" value={description} maxLength={1000} onChange={(e) => setDescription(e.target.value)} required />
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="description" className='inputCard'>Tipo:</label>
+                                    <select id="type" name="type" value={type} onChange={(event) => setType(event.target.value)} required>
+                                        <option value="">Seleccione un tipo</option>
+                                        {types.map((opcion) => (
+                                            <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="race" className='inputCard'>Raza:</label>
+                                    <select id="race" name="race" value={race} onChange={(event) => setRace(event.target.value)} required>
+                                        <option value="">Seleccione una raza</option>
+                                        {races.map((opcion) => (
+                                            <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="cost" className='inputCard'>Costo:</label>
+                                    <input id="number" type='number' min={0} max={100} value={cost} onChange={(e) => setCost(e.target.value)} required />
+                                </div>
+                                <div className='containerInputs'>
+                                    <label htmlFor="energy" className='inputCard'>Energia:</label>
+                                    <input id="number" type='number' min={-100} max={100} value={energy} onChange={(e) => setEnergy(e.target.value)} required />
+                                </div>
+
+                                <button className='button-54' type="submit">Crear carta</button>
+                            </form>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
